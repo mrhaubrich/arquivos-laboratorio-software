69c5397171fda4cc3cf8f5759089f32832a06079
Marco Haubrich <marcolino.haubrich@gmail.com>
2024-03-20 00:35:35 -0300

    Refactor read_file function to include read_csv function

--- src/tests/read_file_test.py
+++ src/tests/read_file_test.py
@@ -6,7 +6,7 @@ from utils.read_file import read_file
 class ReadFileTest(unittest.TestCase):
     """
     1. Dado que o arquivo não existe, então a aplicação deve retornar um erro.
-    3. Dado que o arquivo de clientes existe, então a aplicação deve retornar sucesso.
+    2. Dado que o arquivo de clientes existe, então a aplicação deve retornar sucesso.
     """
     
     def test_read_file_not_exists(self):
@@ -17,5 +17,5 @@ class ReadFileTest(unittest.TestCase):
     def test_read_pagamentos_file(self):
         file = 'tests/pagamentos_file_test.txt'
         # must return the file content
-        self.assertEqual(read_file(file), 'Hello World!')
+        self.assertEqual(read_file(file)[0], 'Hello World!')
         
--- src/utils/read_file.py
+++ src/utils/read_file.py
@@ -1,3 +1,6 @@
 def read_file(file_path: str) -> str:
+    return read_csv(file_path)
+    
+def read_csv(file_path: str) -> list:
     with open(file_path, 'r') as file:
-        return file.read()
+        return file.read().split('\n')
