2a3ed73e87c6f7ab63a89082b9efaa2c3d90e6b3
Marco Haubrich <marcolino.haubrich@gmail.com>
2024-03-20 20:54:39 -0300

    Add CSV reader to Cliente class and implement from_csv method***
    ***Update CSVFileData class to include abstractmethod decorator***
    
    ***Update test_read_file.py to fix file path and assert statement

--- src/classes/cliente.py
+++ src/classes/cliente.py
@@ -1,4 +1,5 @@
 from classes.csv_file_data import CSVFileData
+from csv import reader
 
 
 class Cliente(CSVFileData):
@@ -9,6 +10,27 @@ class Cliente(CSVFileData):
     id: int
     nome: str
 
+    def __init__(self, cliente_id: int, nome: str) -> None:
+        self.id = cliente_id
+        self.nome = nome
+
     @staticmethod
     def from_csv(file: str) -> list["Cliente"]:
-        raise NotImplementedError
+        """
+        Extrai os dados de clientes de um arquivo CSV.
+        """
+        # clientes_csv = "0;0000;0;0;Cliente Sauna;\n1;;;;Cliente 1;"
+        csv_reader = reader(file.split("\n"), delimiter=";")
+        csv_reader = list(csv_reader)
+        
+        if len(csv_reader) == 0:
+            raise ValueError("Dados de clientes vazios ou nulos.")
+        
+        clientes: list[Cliente] = []
+
+        for row in csv_reader:
+            if len(row) != 6:
+                raise ValueError("Dados de clientes incorretos.")
+            clientes.append(Cliente(int(row[0]), row[4]))
+
+        return clientes
--- src/classes/csv_file_data.py
+++ src/classes/csv_file_data.py
@@ -6,8 +6,8 @@ class CSVFileData(metaclass=abc.ABCMeta):
     Classe abstrata que define o comportamento de classes que podem ser instanciadas a partir de um arquivo CSV.
     """
 
-    @abc.abstractmethod
     @staticmethod
+    @abc.abstractmethod
     def from_csv(file: str) -> list["CSVFileData"]:
         """
         Método estático que cria uma instância da classe a partir de um arquivo CSV.
