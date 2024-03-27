6daefbec9014f0d656605ca19cffc15c3298629f
Marco Haubrich <marcolino.haubrich@gmail.com>
2024-03-26 21:19:20 -0300

    Refactor test_filter_lancamentos_nao_pagos.py to use Cliente class in Pagamento objects

--- src/tests/filter/test_filter_lancamentos_nao_pagos.py
+++ src/tests/filter/test_filter_lancamentos_nao_pagos.py
@@ -1,14 +1,15 @@
-from datetime import datetime
 import unittest
+from datetime import datetime
 
+from classes.cliente import Cliente
 from classes.pagamento import Pagamento
 
 
 class TestFilterLancamentosNaoPagos(unittest.TestCase):
     """
-        1. Dado que os dados de pagamentos estão vazio, a funcionalidade deve retornar uma lista vazia.
-        2. Dado que os dados de pagamentos estão nulos, a funcionalidade deve retornar um erro.
-        3. Dado que os dados de pagamentos estão corretos, a funcionalidade deve retornar uma lista de pagamentos não pagos.
+    1. Dado que os dados de pagamentos estão vazio, a funcionalidade deve retornar uma lista vazia.
+    2. Dado que os dados de pagamentos estão nulos, a funcionalidade deve retornar um erro.
+    3. Dado que os dados de pagamentos estão corretos, a funcionalidade deve retornar uma lista de pagamentos não pagos.
     """
 
     def test_filter_lancamentos_nao_pagos_empty_data(self):
@@ -34,22 +35,27 @@ class TestFilterLancamentosNaoPagos(unittest.TestCase):
         Dado que os dados de pagamentos estão corretos, a funcionalidade deve retornar uma lista de pagamentos não pagos.
         """
         date = datetime.now()
+        Cliente.objects = [
+            Cliente(0, "João Silva"),
+            Cliente(1, "Maria Souza"),
+            Cliente(2, "José Santos"),
+        ]
         pagamentos = [
-            Pagamento(0, date, 0, False),
-            Pagamento(1, date, 1, True),
-            Pagamento(2, date, 2, False),
+            Pagamento(Cliente(0, "João Silva"), date, 0, False),
+            Pagamento(Cliente(1, "Maria Souza"), date, 1, True),
+            Pagamento(Cliente(2, "José Santos"), date, 2, False),
         ]
 
         lancamentos_nao_pagos = Pagamento.filter_nao_pagos(pagamentos)
 
         self.assertEqual(len(lancamentos_nao_pagos), 2)
 
-        self.assertEqual(lancamentos_nao_pagos[0].cliente, 0)
+        self.assertEqual(lancamentos_nao_pagos[0].cliente, Cliente(0, "João Silva"))
         self.assertEqual(lancamentos_nao_pagos[0].data, date)
         self.assertEqual(lancamentos_nao_pagos[0].valor, 0)
         self.assertEqual(lancamentos_nao_pagos[0].pago, False)
 
-        self.assertEqual(lancamentos_nao_pagos[1].cliente, 2)
+        self.assertEqual(lancamentos_nao_pagos[1].cliente, Cliente(2, "José Santos"))
         self.assertEqual(lancamentos_nao_pagos[1].data, date)
         self.assertEqual(lancamentos_nao_pagos[1].valor, 2)
         self.assertEqual(lancamentos_nao_pagos[1].pago, False)
