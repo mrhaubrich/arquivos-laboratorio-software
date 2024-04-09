ba3b98d7e206d1a9a2315d1cc03cea90d7f7a13c
Marco Haubrich <marhaubrich@ucs.br>
2024-04-09 17:18:18 -0300

    Refactor admin.py to register Cliente and Pagamento models in Django admin

--- src/projeto_django/lab_software/admin.py
+++ src/projeto_django/lab_software/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from lab_software.models import Cliente, Pagamento
+
 # Register your models here.
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome")
+    search_fields = ("id", "nome")
+
+
+class PagamentoAdmin(admin.ModelAdmin):
+    list_display = ("cliente", "data", "valor")
+    search_fields = ("cliente", "data", "valor")
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Pagamento, PagamentoAdmin)
